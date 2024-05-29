cefc21b2c4982587977abfa7417b9ad0eb4723d2
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-05-29 14:33:11 +0530

    Initial Commit

---
+++ qazxsw/qazxswApp.swift
@@ -0,0 +1,17 @@
+//
+//  qazxswApp.swift
+//  qazxsw
+//
+//  Created by Test Ios on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct qazxswApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
